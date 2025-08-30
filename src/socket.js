c93244d8166f2abbbce4fae1ee19d53a99d261b8
Anoop555777 <ajbisht99@gmail.com>
2025-08-30 13:30:25 +0530

    final

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 //
-const socket = io("http://localhost:8000", {
+const socket = io("https://ajchat-pmbk.onrender.com", {
   withCredentials: true,
   autoConnect: true,
   transports: ["websocket"],
